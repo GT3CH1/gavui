722f8dc7b4cb59200623fe0cac6ac513977eb5f3
Gavin Pease <gavinpease@gmail.com>
2023-01-07 09:58:58 -0700

    Simplify rendering engine

--- src/main/java/com/peasenet/gavui/util/GuiUtil.java
+++ src/main/java/com/peasenet/gavui/util/GuiUtil.java
@@ -21,128 +21,95 @@
 package com.peasenet.gavui.util;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.peasenet.gavui.color.Color;
 import com.peasenet.gavui.math.BoxF;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import org.joml.Matrix4f;
 
-
 /**
  * @author gt3ch1
- * @version 7/13/2022
+ * @version 01/07/2023
  * A utility class for drawing gui elements.
  */
 public class GuiUtil {
     /**
-     * @param acColor     - The color to draw the box with.
+     * Draws a box around the given box, with an alpha of 1f.
+     *
+     * @param c           - The color to draw the box with.
      * @param box         - The box to draw.
      * @param matrixStack - The matrix stack to draw with.
      */
-    public static void drawBox(float[] acColor, BoxF box, MatrixStack matrixStack) {
-        drawBox(acColor, box, matrixStack, GavUISettings.getFloat("gui.alpha"));
+    public static void drawBox(Color c, BoxF box, MatrixStack matrixStack) {
+        drawBox(c, box, matrixStack, 1f);
     }
 
     /**
-     * @param acColor     - The color to draw the box with.
+     * @param c           - The color to draw the box with.
      * @param box         - The box to draw.
      * @param matrixStack - The matrix stack to draw with.
+     * @param alpha       - The alpha value to draw with.
      */
-    public static void drawBox(float[] acColor, BoxF box, MatrixStack matrixStack, float alpha) {
-        float xt1 = (float) box.getTopLeft().x();
-        float yt1 = (float) box.getTopLeft().y();
-        float xt2 = (float) box.getBottomRight().x();
-        float yt2 = (float) box.getBottomRight().y();
+    public static void drawBox(Color c, BoxF box, MatrixStack matrixStack, float alpha) {
+        var acColor = c.getAsFloatArray();
         RenderSystem.setShader(GameRenderer::getPositionProgram);
         RenderSystem.enableBlend();
-        var matrix = matrixStack.peek().getPositionMatrix();
-        var bufferBuilder = Tessellator.getInstance().getBuffer();
         RenderSystem.setShaderColor(acColor[0], acColor[1], acColor[2], alpha);
-        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
-        bufferBuilder.vertex(matrix, xt1, yt1, 0).next();
-        bufferBuilder.vertex(matrix, xt1, yt2, 0).next();
-        bufferBuilder.vertex(matrix, xt2, yt2, 0).next();
-        bufferBuilder.vertex(matrix, xt2, yt1, 0).next();
-        bufferBuilder.vertex(matrix, xt1, yt1, 0).next();
-        Tessellator.getInstance().draw();
-    }
 
-    /**
-     * Draws a box with the given coordinates.
-     *
-     * @param xt1           - The x coordinate of the top left corner.
-     * @param yt1           - The y coordinate of the top left corner.
-     * @param xt2           - The x coordinate of the bottom right corner.
-     * @param yt2           - The y coordinate of the bottom right corner.
-     * @param matrix        - The matrix to draw with.
-     * @param bufferBuilder - The buffer to draw with.
-     */
-    public static void drawBox(int xt1, int yt1, int xt2, int yt2, Matrix4f matrix, BufferBuilder bufferBuilder) {
-
-
-    }
-
-    /**
-     * Draws a box on screen.
-     *
-     * @param acColor     The color of the box as a 4 point float array.
-     * @param xt1         The x coordinate of the top left corner of the box.
-     * @param yt1         The y coordinate of the top left corner of the box.
-     * @param xt2         The x coordinate of the bottom right corner of the box.
-     * @param yt2         The y coordinate of the bottom right corner of the box.
-     * @param matrixStack The matrix stack used to draw boxes on screen.
-     */
-    public static void drawBox(float[] acColor, int xt1, int yt1, int xt2, int yt2, MatrixStack matrixStack) {
-        RenderSystem.setShader(GameRenderer::getPositionProgram);
-        RenderSystem.enableBlend();
-        var matrix = matrixStack.peek().getPositionMatrix();
         var bufferBuilder = Tessellator.getInstance().getBuffer();
-        RenderSystem.setShaderColor(acColor[0], acColor[1], acColor[2], GavUISettings.getFloat("gui.alpha"));
         bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
-        drawBox(xt1, yt1, xt2, yt2, matrix, bufferBuilder);
-        Tessellator.getInstance().draw();
+        var matrix = matrixStack.peek().getPositionMatrix();
+        drawBox(box, matrix, bufferBuilder);
+    }
+
+    /**
+     * Draws an outline of the given box with the given color, with an alpha of 1f.
+     *
+     * @param c           - The color to draw the outline with.
+     * @param box         - The outline of a box to draw.
+     * @param matrixStack - The matrix stack to draw with.
+     */
+    public static void drawOutline(Color c, BoxF box, MatrixStack matrixStack) {
+        drawOutline(c, box, matrixStack, 1.0f);
     }
 
     /**
      * Draws an outline of the given box with the given color.
      *
-     * @param acColor     - The color to draw the outline with.
+     * @param c           - The color to draw the outline with.
      * @param box         - The outline of a box to draw.
      * @param matrixStack - The matrix stack to draw with.
+     * @param alpha       - The alpha value to draw with.
      */
-    public static void drawOutline(float[] acColor, BoxF box, MatrixStack matrixStack) {
+    public static void drawOutline(Color c, BoxF box, MatrixStack matrixStack, float alpha) {
+        var acColor = c.getAsFloatArray();
         RenderSystem.setShader(GameRenderer::getPositionProgram);
         RenderSystem.enableBlend();
+        RenderSystem.setShaderColor(acColor[0], acColor[1], acColor[2], alpha);
+
         var matrix = matrixStack.peek().getPositionMatrix();
         var bufferBuilder = Tessellator.getInstance().getBuffer();
-        RenderSystem.setShaderColor(acColor[0], acColor[1], acColor[2], 1.0F);
         bufferBuilder.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION);
+        drawBox(box, matrix, bufferBuilder);
+    }
 
-        var xt1 = (float) box.getTopLeft().x();
-        var yt1 = (float) box.getTopLeft().y();
-        var xt2 = (float) box.getBottomRight().x();
-        var yt2 = (float) box.getBottomRight().y();
+    /**
+     * Draws a box around the given box.
+     *
+     * @param box           - The box to draw.
+     * @param matrix        - The matrix to draw with.
+     * @param bufferBuilder - The buffer builder to draw with.
+     */
+    private static void drawBox(BoxF box, Matrix4f matrix, BufferBuilder bufferBuilder) {
+        var xt1 = box.getTopLeft().x();
+        var yt1 = box.getTopLeft().y();
+        var xt2 = box.getBottomRight().x();
+        var yt2 = box.getBottomRight().y();
         bufferBuilder.vertex(matrix, xt1, yt1, 0).next();
         bufferBuilder.vertex(matrix, xt1, yt2, 0).next();
         bufferBuilder.vertex(matrix, xt2, yt2, 0).next();
         bufferBuilder.vertex(matrix, xt2, yt1, 0).next();
         bufferBuilder.vertex(matrix, xt1, yt1, 0).next();
         Tessellator.getInstance().draw();
-
-    }
-
-
-    /**
-     * Tessellator.getInstance().draw();                                                       * Draws an outline of the given coordinates with the given color.
-     *
-     * @param acColor     - The color to draw the outline with.
-     * @param xt1         - The x coordinate of the top left corner of the box.
-     * @param yt1         - The y coordinate of the top left corner of the box.
-     * @param xt2         - The x coordinate of the bottom right corner of the box.
-     * @param yt2         - The y coordinate of the bottom right corner of the box.
-     * @param matrixStack - The matrix stack to draw with.
-     */
-    public static void drawOutline(float[] acColor, int xt1, int yt1, int xt2, int yt2, MatrixStack matrixStack) {
-
-
     }
 }
